4b2350ba72fde88045c7f6ea392c0e3cbc85d84e
RapidBlaze53 <127673703+RapidBlaze53@users.noreply.github.com>
2023-09-29 20:11:32 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDwSsy4pwUMlROP-sAIFEWcW6Nd6tyy7xg",
+    authDomain: "socialwebsite-616eb.firebaseapp.com",
+    databaseURL: "https://socialwebsite-616eb-default-rtdb.firebaseio.com",
+    projectId: "socialwebsite-616eb",
+    storageBucket: "socialwebsite-616eb.appspot.com",
+    messagingSenderId: "58479220655",
+    appId: "1:58479220655:web:e328883f5e32549d93015a"
+  };
+
+  function logout() {
+    window.location = "index.html";
+  }
